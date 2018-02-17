28ddc81236c04d384bbd523e4ba5b97ba0a41710
Flávio Diniz Rossetto Júnior <flaviodrossetto@gmail.com>
2018-02-17 13:21:30 -0200

    finalizando boilerplate dos testes

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,6 +1,5 @@
 import { expect } from 'chai';
 import * as mocha from 'mocha';
-
 describe('Main', () => {
   context('Method Check Mocha and Chai',() => {
     it('shoud prove mocha and chai is installed and running', function(){
